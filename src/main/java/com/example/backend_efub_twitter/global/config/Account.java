02dbd4c3190ef448a4a7463a246d8907a4820487
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2022-05-09 02:56:56 +0900

    feat: add token based security

---
+++ src/main/java/com/example/backend_efub_twitter/global/config/Account.java
@@ -0,0 +1,61 @@
+package com.example.backend_efub_twitter.global.config;
+
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+
+@NoArgsConstructor
+public class Account implements UserDetails {
+
+	private String email;
+	private String password;
+	private String role;
+
+	@Builder
+	public Account(String email, String password, String role) {
+		this.email = email;
+		this.password = password;
+		this.role = "ROLE_" + role;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Collections.singletonList(new SimpleGrantedAuthority(this.role));
+	}
+
+	@Override
+	public String getPassword() {
+		return this.password;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+}
