886d35485078d9f538928d87af96d827ca9af967
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2022-05-11 00:16:01 +0900

    feat: remove board info from profile

--- src/main/java/com/example/backend_efub_twitter/domain/profiile/domain/ProfileResDto.java
+++ src/main/java/com/example/backend_efub_twitter/domain/profiile/domain/ProfileResDto.java
@@ -1,24 +1,19 @@
 package com.example.backend_efub_twitter.domain.profiile.domain;
 
-import com.example.backend_efub_twitter.domain.board.entity.Board;
 import com.example.backend_efub_twitter.domain.profiile.entity.Profile;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @Getter
 @NoArgsConstructor
 public class ProfileResDto {
 	String nickname;
 	String bio;
-	List<Board> boardList;
 
 	@Builder
-	public ProfileResDto(Profile profile, List<Board> boardList){
+	public ProfileResDto(Profile profile){
 		this.nickname = profile.getNickname();
 		this.bio = profile.getBio();
-		this.boardList = boardList;
 	}
 }
--- src/main/java/com/example/backend_efub_twitter/domain/profiile/service/ProfileService.java
+++ src/main/java/com/example/backend_efub_twitter/domain/profiile/service/ProfileService.java
@@ -1,9 +1,6 @@
 package com.example.backend_efub_twitter.domain.profiile.service;
 
-import com.example.backend_efub_twitter.domain.board.entity.Board;
-import com.example.backend_efub_twitter.domain.board.repository.BoardRepository;
 import com.example.backend_efub_twitter.domain.profiile.domain.ProfileModifyReqDto;
-import com.example.backend_efub_twitter.domain.profiile.domain.ProfileModifyResDto;
 import com.example.backend_efub_twitter.domain.profiile.domain.ProfileResDto;
 import com.example.backend_efub_twitter.domain.profiile.entity.Profile;
 import com.example.backend_efub_twitter.domain.profiile.exception.DuplicateNicknameException;
@@ -14,16 +11,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
 @Service
 @RequiredArgsConstructor
 public class ProfileService {
 
 	private final ProfileRepository profileRepository;
-	private final BoardRepository boardRepository;
 
 	@Transactional(readOnly = true)
 	public ProfileResDto getProfile(String nickname) {
@@ -33,16 +25,12 @@ public class ProfileService {
 
 		User user = profile.getUser();
 
-		Optional<Board> optionalBoardList = boardRepository.findByUser_Id(user.getId());
-		List<Board> boardList = optionalBoardList.stream().collect(Collectors.toList());
-
 		return ProfileResDto.builder()
 			.profile(profile)
-			.boardList(boardList)
 			.build();
 	}
 
-	public ProfileModifyResDto modifyProfile(String nickname, ProfileModifyReqDto profileModifyReqDto) {
+	public ProfileResDto modifyProfile(String nickname, ProfileModifyReqDto profileModifyReqDto) {
 
 		if (profileRepository.existsByNickname(profileModifyReqDto.getNickname()))
 			throw new DuplicateNicknameException("이미 존재하는 닉네임입니다.");
@@ -57,10 +45,8 @@ public class ProfileService {
 
 		profileRepository.save(profile);
 
-		return ProfileModifyResDto.builder()
-			.fullname(user.getFullName())
-			.nickname(profile.getNickname())
-			.bio(profile.getBio())
+		return ProfileResDto.builder()
+			.profile(profile)
 			.build();
 	}
 }
